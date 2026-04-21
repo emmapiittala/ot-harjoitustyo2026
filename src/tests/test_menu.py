6258d023e97b3ec80a27761d9203da71ad97b553
Emma Piittala <piittala@ad.helsinki.fi>
2026-04-21 20:01:59 +0300

    lisätty uudet testit

--- src/tests/test_menu.py
+++ src/tests/test_menu.py
@@ -1,65 +1,31 @@
 import unittest
-from ot_harjoitustyo2026.menu import Menu
 from tkinter import Tk
-from ot_harjoitustyo2026.ui import UI
+from ot_harjoitustyo2026.logic import GameLogic
 
 
 class TestMenu(unittest.TestCase):
     def setUp(self):
-       self.root = Tk()
-       self.root.withdraw()
-       window = Tk()
+        questions = [
+        {"question": "Toimiiko kysymys?(Kyllä, joo ja oikein toimivat)",
+        "choices": ["Kyllä","Ei,","Ehkä,","joo",
+        "Voipi olla,","eitietenkään","väärä","oikein","mietitään","jeepuls"],
+        "correct_answers" :["Kyllä","joo","oikein"]
+        }
+        ]
+        self.logic = GameLogic(questions)
 
-    def tearDown(self):
-        self.root.destroy()
-        
-    def handle_start(self):
-        pass
-    
-    def current_view(self):
-        pass
-    
-    def test_menu_creating(self):
-        menu = Menu(self.root, self.handle_start)
-        self.assertIsNotNone(menu)
-    
-    def test_menu_destroy(self):
-        menu = Menu(self.root, self.handle_start)
-        menu.destroy()
+
+    def test_check_answer_return_correct_answer(self):
+        result = self.logic.check_answer(["Kyllä"])
         self.assertTrue(True)
-        
-    def test_ui_start(self):
-        ui = UI(self.root)
-        ui.start()
+
+    def test_check_anwer_is_not_correct(self):
+        result = self.logic.check_answer(["Ei"])
+        self.assertFalse(False)
+
+    def test_show_question(self):
+        result = self.logic.get_question()
         self.assertTrue(True)
+
+
     
-    def test_ui_current_view(self):
-        ui = UI(self.root)
-        ui.start()
-        self.assertIsNotNone(ui._current_view)
-        
-    def test_ui_show_menu(self):
-        ui = UI(self.root)
-        ui.start()
-        ui._show_menu()
-        ui._show_game()
-        self.assertIsNotNone(ui._current_view)
-        
-    def test_ui_show_game(self):
-        ui = UI(self.root)
-        ui.start()
-        ui._show_game()
-        ui._show_menu()
-        self.assertTrue(True)
-        
-    def test_ui_clear_view(self):
-        ui = UI(self.root)     
-        ui.start()
-        ui._clear_view()
-        self.assertTrue(True)
-        
-    def test_ui_cleer_view_false(self):
-        ui = UI(self.root)
-        ui._clear_view()
-        self.assertIsNone(ui._current_view)
-        
