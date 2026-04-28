3e84cfb9f1ba6e3ed22bd4d7d72530191f8550c7
Emma Piittala <piittala@ad.helsinki.fi>
2026-04-28 17:51:42 +0300

    korjattu tasks.py

--- Visailupeli/tasks.py
+++ Visailupeli/tasks.py
@@ -5,9 +5,21 @@ def start(ctx):
     ctx.run("python3 src/main.py", pty=True)
     
 @task
+def test(ctx):
+    ctx.run("pytest src/tests", pty=True)
+    
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
+    
+@task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
+    
+
+
+    
