2d4c323a8101bac2f07a5f6b389420f28d9a98c0
Emma Piittala <piittala@ad.helsinki.fi>
2026-04-28 21:41:31 +0300

    Lisää testejä ja vielä pylint säätämistä

--- Visailupeli/src/tests/test_menu.py
+++ Visailupeli/src/tests/test_menu.py
@@ -10,8 +10,17 @@ class TestMenu(unittest.TestCase):
         "choices": ["Kyllä","Ei,","Ehkä,","joo",
         "Voipi olla,","eitietenkään","väärä","oikein","mietitään","jeepuls"],
         "correct_answers" :["Kyllä","joo","oikein"]
-        }
-        ]
+        },
+        {"question": "Toimiiko kysymys 2? ja vaihtuuko vastaukset (oikeata k ja voipi oll)",
+     "choices": ["k","E,","Ehk,","juu",
+                 "Voipi oll","eitiete","vääräp","oikexin","mietitäänstten","jeepulsjee"],
+     "correct_answers" :["k","Voipi oll"]
+     },     {"question": "Toimiiko kysymys 3? (yksi vain oikein a)",
+     "choices": ["a","b","c","d",
+                 "e,","f","g","h","i","j"],
+     "correct_answers" :["a"]
+     }
+    ]
         self.logic = GameLogic(questions)
 
 
@@ -19,7 +28,7 @@ class TestMenu(unittest.TestCase):
         result = self.logic.check_answer(["Kyllä"])
         self.assertTrue(result)
 
-    def test_check_anwer_is_not_correct(self):
+    def test_check_answer_is_not_correct(self):
         result = self.logic.check_answer(["Ei"])
         self.assertFalse(result)
 
@@ -27,5 +36,15 @@ class TestMenu(unittest.TestCase):
         result = self.logic.get_question()
         self.assertTrue(result)
 
-
-    
+    def test_no_answer(self):
+        result = self.logic.check_answer([])
+        self.assertFalse(result)
+        
+    def test_next_question(self):
+        result = self.logic.current_question
+        self.logic.next_question()
+        self.assertEqual(self.logic.current_question, result + 1)
+        
+    def test_last_question(self):
+        self.logic.current_question = len(self.logic.questions) - 1
+        self.assertFalse(self.logic.check_questions())
